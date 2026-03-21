84acea1f96ffb4896e73bc9d609eecea07a56718
Bhushan <bhushanp721@gmail.com>
2026-03-21 16:33:45 +0530

    work for error

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::middleware('auth')->group(function () {
     route::post('user/change-password',[ProfileController::class,'changePassword'])->name('user.change-password');
 });
 
-Route::get('/test', function () {
+Route::get('/testtest', function (xcxc) {
     creturn view('welcome');sdfsdf
 });
 require __DIR__.'/auth.php';
