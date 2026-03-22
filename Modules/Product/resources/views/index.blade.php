b12632009cb1ca82dc2136e9d3fa4d90c4080510
Bhushan <bhushanp721@gmail.com>
2026-03-22 17:00:07 +0530

    lorm ipsum add

--- Modules/Product/resources/views/index.blade.php
+++ Modules/Product/resources/views/index.blade.php
@@ -1,5 +1,8 @@
 <x-product::layouts.master>
     <h1>Hello World</h1>
+    <p
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda commodi cumque deserunt dignissimos </p>
 
     <p>Module: {!! config('product.name') !!}</p>
 </x-product::layouts.master>
