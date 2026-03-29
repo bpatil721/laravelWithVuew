84872bbf1643d3807b90860cc1585a447a5a5f80
Bhushan <bhushanp721@gmail.com>
2026-03-29 17:01:49 +0530

    work on fix

--- Modules/Product/resources/views/index.blade.php
+++ Modules/Product/resources/views/index.blade.php
@@ -1,8 +1,6 @@
 <x-product::layouts.master>
     <h1>Hello World</h1>
-    <p
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda commodi cumque deserunt dignissimos </p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda commodi cumque deserunt dignissimos </p>
 
     <p>Module: {!! config('product.name') !!}</p>
 </x-product::layouts.master>
